1a8324f357748160414116f5d17071b62eaeb728
anass bouirdi <anassbouirdi@anasss-MBP.lan>
2025-07-01 20:44:33 +0200

    quick fix

--- deploy_databricks.sh
+++ deploy_databricks.sh
@@ -1,13 +1,6 @@
 # deploy_databricks.sh
 #!/bin/bash
 
-# Vérifier que le Databricks CLI est installé, sinon l'installer
-if ! command -v databricks &> /dev/null; then
-  echo "Databricks CLI non trouvé, installation..."
-  pip install --user databricks-cli
-  export PATH="$HOME/.local/bin:$PATH"
-fi
-
 # S'assurer que DATABRICKS_INSTANCE commence par https://
 if [[ "$DATABRICKS_INSTANCE" != https://* ]]; then
   export DATABRICKS_INSTANCE="https://${DATABRICKS_INSTANCE}"
