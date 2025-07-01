a921fc7b98b83c7178055190c00370a6a615eae8
anass bouirdi <anassbouirdi@anasss-MBP.lan>
2025-07-01 22:56:23 +0200

    tests unit

---
+++ tests/test_train.py
@@ -0,0 +1,31 @@
+import pytest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import train
+from unittest import mock
+from sklearn.linear_model import LinearRegression
+
+@mock.patch("mlflow.sklearn.log_model")
+@mock.patch("mlflow.log_metric")
+@mock.patch("mlflow.start_run")
+@mock.patch("mlflow.set_experiment")
+def test_train_main_mocks(mock_set_experiment, mock_start_run, mock_log_metric, mock_log_model):
+    # On mocke le contexte de run MLflow
+    mock_start_run.return_value.__enter__.return_value = None
+
+    # Exécute le main
+    train.main()
+
+    # Vérifie que l'expérience MLflow est bien définie
+    mock_set_experiment.assert_called_once_with("diabetes-linear-regression")
+    # Vérifie que le log du score a été appelé
+    assert mock_log_metric.call_count == 1
+    # Vérifie que le modèle a bien été loggué
+    assert mock_log_model.call_count == 1
+
+# def test_train_runs_without_error():
+#     try:
+#         train.main()
+#     except Exception as e:
+#         pytest.fail(f"train.main() raised an exception: {e}") 
